56f93d4e3003f9ef98ffec9a895bbfe3f6218b86
YaoShangTseng <yakushou730@gmail.com>
2016-06-17 15:12:18 +0800

    finish Message basic CRUD

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,57 @@
 class MessagesController < ApplicationController
+
+  before_action :set_message, :only => [:show, :edit, :update, :destroy]
+
+  def index
+    @messages = Message.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      flash[:notice] = "建立成功"
+      redirect_to messages_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @message.update(message_params)
+      flash[:notice] = "更新成功"
+      redirect_to messages_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @message.destroy
+    flash[:alert] = "刪除成功"
+    redirect_to messages_path
+  end
+
+
+  protected
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
+  def message_params
+    params.require(:message).permit(:title, :content)
+  end
+
 end
