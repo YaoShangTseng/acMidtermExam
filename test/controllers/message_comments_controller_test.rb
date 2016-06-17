0c6bbe29bdcb339127ccd33347e7a2d6ad58fb46
YaoShangTseng <yakushou730@gmail.com>
2016-06-17 15:23:13 +0800

    rename controller comments to message_comments

--- test/controllers/message_comments_controller_test.rb
+++ test/controllers/message_comments_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CommentsControllerTest < ActionController::TestCase
+class MessageCommentsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
